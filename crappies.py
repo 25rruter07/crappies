8f02018da698a0a782f95d8a8e7fc9557eca845c
Zaboss1107 <149806284+Zaboss1107@users.noreply.github.com>
2024-04-11 09:33:32 -0500

    Update crappies.py

--- crappies.py
+++ crappies.py
@@ -1,6 +1,6 @@
 
 ##############################################################################
-#   p324_honey_production_3.py
+#   p324_Catch_production_3.py
 #  
 #      create first graph with all counties - this results in too much data
 #      to analyze
@@ -23,69 +23,69 @@ df.dropna(subset=['Value'], inplace=True)
 unique_counties = df['county'].unique()
 
 # parse and store pandas data
-all_honey = []
+all_Catch = []
 all_counties = []
 for county in unique_counties:
   # match this county in the county column, 
   # group each years worth of data for that county,
   # and retreive values from the Value column
-  honey_data =  df[df['county'] == county].groupby('Year')['Value']
+  Catch_data =  df[df['county'] == county].groupby('Year')['Value']
   # sum the values and store in an array
-  all_honey.append(honey_data.sum())
+  all_Catch.append(Catch_data.sum())
   # store the county
   all_counties.append(county)
 
 # show all counties - too much data!
-for i in range(len(all_honey)):
-  # get the county and its honey values
-  honey = all_honey[i]
+for i in range(len(all_Catch)):
+  # get the county and its Catch values
+  Catch = all_Catch[i]
   county = all_counties[i]
-  # the keys in the honey data structure are the years
-  years = honey.keys()
-  '''if sum(honey) < small_vol:
-    plt.plot(honey.keys(), honey, label=county, marker="o")
-    plt.plot(honey.keys(), honey, label=county)
+  # the keys in the Catch data structure are the years
+  years = Catch.keys()
+  '''if sum(Catch) < small_vol:
+    plt.plot(Catch.keys(), Catch, label=county, marker="o")
+    plt.plot(Catch.keys(), Catch, label=county)
 plt.xlabel('Year')
 plt.ylabel('Production levels')
-plt.title('LOW HONEY PRODUCERS')
+plt.title('LOW Catch PRODUCERS')
 plt.legend()
 plt.show()'''
 
-for i in range(len(all_honey)):
-  # get the county and its honey values
-  honey = all_honey[i]
+for i in range(len(all_Catch)):
+  # get the county and its Catch values
+  Catch = all_Catch[i]
   county = all_counties[i]
-  # the keys in the honey data structure are the years
-  years = honey.keys()
-  '''if sum(honey) <= mid_vol and sum(honey) >= small_vol:
-    plt.plot(honey.keys(), honey, label=county, marker="o")
-    plt.plot(honey.keys(), honey, label=county)
+  # the keys in the Catch data structure are the years
+  years = Catch.keys()
+  '''if sum(Catch) <= mid_vol and sum(Catch) >= small_vol:
+    plt.plot(Catch.keys(), Catch, label=county, marker="o")
+    plt.plot(Catch.keys(), Catch, label=county)
 plt.xlabel('Year')
 plt.ylabel('Production levels')
-plt.title('MID-LEVEL HONEY PRODUCERS')
+plt.title('MID-LEVEL Catch PRODUCERS')
 plt.legend()
 plt.show()'''
 
-for i in range(len(all_honey)):
-  # get the county and its honey values
-  honey = all_honey[i]
+for i in range(len(all_Catch)):
+  # get the county and its Catch values
+  Catch = all_Catch[i]
   county = all_counties[i]
-  # the keys in the honey data structure are the years
-  years = honey.keys()
-'''if sum(honey) > mid_vol:
-    plt.plot(honey.keys(), honey, label=county, marker="o")
-    plt.plot(honey.keys(), honey, label=county)
+  # the keys in the Catch data structure are the years
+  years = Catch.keys()
+'''if sum(Catch) > mid_vol:
+    plt.plot(Catch.keys(), Catch, label=county, marker="o")
+    plt.plot(Catch.keys(), Catch, label=county)
 plt.xlabel('Year')
 plt.ylabel('Production levels')
-plt.title('LARGE HONEY PRODUCERS')
+plt.title('LARGE Catch PRODUCERS')
 plt.legend()
 plt.show()'''
 
 for county in unique_counties:
-  honey_data = df[df['county'] == county].groupby('Year')['Value']
-  honey_mean = honey_data.mean()
-  plt.plot(honey_mean.keys(), honey_mean, label=county, marker="o")
-  plt.plot(honey_mean.keys(), honey_mean, label=county)
+  Catch_data = df[df['county'] == county].groupby('Year')['Value']
+  Catch_mean = Catch_data.mean()
+  plt.plot(Catch_mean.keys(), Catch_mean, label=county, marker="o")
+  plt.plot(Catch_mean.keys(), Catch_mean, label=county)
 plt.ylabel('Production Levels')
 plt.xlabel('Year')
 plt.title('county AVERAGES')
